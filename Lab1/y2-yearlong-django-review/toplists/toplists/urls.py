4502cfae5533bd1e1f5a0fdb273f052d7df347b9
Costas Mustaklem <costas12@meet.mit.edu>
2013-10-20 18:35:13 +0200

    Task 1 done

--- Lab1/y2-yearlong-django-review/toplists/toplists/urls.py
+++ Lab1/y2-yearlong-django-review/toplists/toplists/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, include, url
 from movies import views
 
 urlpatterns = patterns('', 
+    # url(r'',views.movie_list),
     url(r'^movies$', views.movie_list),
     url(r'^movies/home$', views.movie_list),
     # this one tells views.py to process adding a movie
